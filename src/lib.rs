7ca8a682cb1a1c4e6bc93149514555c2bb52ba39
makanai5610 <takumi.k.5610@gmail.com>
2020-03-17 01:35:41 +0900

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -25,7 +25,7 @@ pub fn start() -> Result<(), JsValue> {
             vec2 zeroToOne = position / resolution;
             vec2 zeroToTwo = zeroToOne * 2.0;
             vec2 clipSpace = zeroToTwo - 1.0;
-            gl_Position = vec4(clipSpace, 0, 1);
+            gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
         }
     "#,
     )?;
