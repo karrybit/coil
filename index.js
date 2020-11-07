973814840da66c3d8d88a1e26e989c5cda9bd4f1
makanai5610 <takumi.k.5610@gmail.com>
2020-11-07 16:21:20 +0900

    fix

--- index.js
+++ index.js
@@ -1,14 +1,64 @@
 let pkg = import("./pkg").catch(console.error);
 let WASM = undefined;
-let buf = undefined;
+let jsBuf = undefined;
+let wasmBuf = undefined;
+let count = 0;
 
 window.transition = async function transition() {
   if (!WASM) {
     WASM = await pkg;
   }
-  if (!buf) {
-    const image = await fetch('https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/480px-Unofficial_JavaScript_logo_2.svg.png');
-    buf = new Uint8Array(await (await image.blob()).arrayBuffer());
+  let buf = undefined;
+  if (count % 2 == 0) {
+    if (!jsBuf) {
+      const image = await fetch('https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/480px-Unofficial_JavaScript_logo_2.svg.png');
+      jsBuf = new Uint8Array(await (await image.blob()).arrayBuffer());
+    }
+    buf = jsBuf;
+  } else {
+    if (!wasmBuf) {
+      const image = await fetch('https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/WebAssembly_Logo.svg/180px-WebAssembly_Logo.svg.png');
+      wasmBuf = new Uint8Array(await (await image.blob()).arrayBuffer());
+    }
+    buf = wasmBuf;
   }
-  WASM.transition(buf);
+  WASM.transition(buf, false);
+  count++;
 };
+
+window.revTransition = async function revTransition() {
+  if (!WASM) {
+    WASM = await pkg;
+  }
+  let buf = undefined;
+  if (count % 2 == 0) {
+    if (!jsBuf) {
+      const image = await fetch('https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/480px-Unofficial_JavaScript_logo_2.svg.png');
+      jsBuf = new Uint8Array(await (await image.blob()).arrayBuffer());
+    }
+    buf = jsBuf;
+  } else {
+    if (!wasmBuf) {
+      const image = await fetch('https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/WebAssembly_Logo.svg/180px-WebAssembly_Logo.svg.png');
+      wasmBuf = new Uint8Array(await (await image.blob()).arrayBuffer());
+    }
+    buf = wasmBuf;
+  }
+  WASM.transition(buf, true);
+  count++;
+};
+
+window.pager = async function pager() {
+  if (!WASM) {
+    WASM = await pkg;
+  }
+  if (!jsBuf) {
+    const image = await fetch('https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/480px-Unofficial_JavaScript_logo_2.svg.png');
+    jsBuf = new Uint8Array(await (await image.blob()).arrayBuffer());
+  }
+  const buf = jsBuf;
+  const { Pager } = WASM
+  const pager = new Pager(buf, false);
+  console.log(pager);
+  pager.log();
+}
--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,12 @@ extern "C" {
     fn log(s: &str);
 }
 
+#[wasm_bindgen]
+pub struct Pager {
+    canvas: web_sys::HtmlCanvasElement,
+    context: WebGlRenderingContext,
+}
+
 // #[wasm_bindgen(start)]
 // pub async fn start() -> Result<(), JsValue> {
 //     let wasm_image = loader::fetch_image(loader::FetchURLType::WASM).await;
@@ -152,7 +158,69 @@ extern "C" {
 // }
 
 #[wasm_bindgen]
-pub async fn transition(data: Vec<u8>) -> Result<(), JsValue> {
+impl Pager {
+    #[wasm_bindgen(constructor)]
+    pub fn new(defaultImage: Vec<u8>, isFixCanvasSize: bool) -> Result<Pager, JsValue> {
+        let window = web_sys::window().unwrap();
+        let document = window.document().unwrap();
+        let canvas = document.get_element_by_id("canvas").unwrap();
+        let canvas: web_sys::HtmlCanvasElement = canvas.dyn_into::<web_sys::HtmlCanvasElement>()?;
+
+        let img = image::load_from_memory(&defaultImage);
+        let img = img.unwrap();
+        let (width, height) = img.dimensions();
+        let (width, height) = (canvas.width(), canvas.height());
+        if !isFixCanvasSize {
+            canvas.set_width(width);
+            canvas.set_height(height);
+        }
+
+        let context = canvas
+            .get_context("webgl")?
+            .unwrap()
+            .dyn_into::<WebGlRenderingContext>()?;
+
+        let vert_shader = shader::new_vertex_shader(&context)?;
+        let frag_shader = shader::new_fragment_shader(&context)?;
+        let program = processor::link_program(&context, &vert_shader, &frag_shader)?;
+        context.use_program(Some(&program));
+
+        let texture = context.create_texture();
+        context.bind_texture(WebGlRenderingContext::TEXTURE_2D, texture.as_ref());
+
+        context.tex_parameteri(
+            WebGlRenderingContext::TEXTURE_2D,
+            WebGlRenderingContext::TEXTURE_WRAP_S,
+            WebGlRenderingContext::CLAMP_TO_EDGE as i32,
+        );
+        context.tex_parameteri(
+            WebGlRenderingContext::TEXTURE_2D,
+            WebGlRenderingContext::TEXTURE_WRAP_T,
+            WebGlRenderingContext::CLAMP_TO_EDGE as i32,
+        );
+        context.tex_parameteri(
+            WebGlRenderingContext::TEXTURE_2D,
+            WebGlRenderingContext::TEXTURE_MIN_FILTER,
+            WebGlRenderingContext::NEAREST as i32,
+        );
+        context.tex_parameteri(
+            WebGlRenderingContext::TEXTURE_2D,
+            WebGlRenderingContext::TEXTURE_MAG_FILTER,
+            WebGlRenderingContext::NEAREST as i32,
+        );
+
+        Ok(Pager{context: context, canvas: canvas})
+    }
+
+    pub fn log(&self) {
+        unsafe {
+            log(";lasdkfj;lsadfjlasd");
+        }
+    }
+}
+
+#[wasm_bindgen]
+pub async fn transition(data: Vec<u8>, rev: bool) -> Result<(), JsValue> {
     let window = web_sys::window().unwrap();
     let document = window.document().unwrap();
     let canvas = document.get_element_by_id("canvas").unwrap();
@@ -161,31 +229,23 @@ pub async fn transition(data: Vec<u8>) -> Result<(), JsValue> {
     let img = image::load_from_memory(&data);
     let img = img.unwrap();
     let (width, height) = img.dimensions();
-
-    canvas.set_width(width);
-    canvas.set_height(height);
+    let (width, height) = (canvas.width(), canvas.height());
+    // canvas.set_width(width);
+    // canvas.set_height(height);
 
     let context = canvas
         .get_context("webgl")?
         .unwrap()
         .dyn_into::<WebGlRenderingContext>()?;
 
-    // vertex shaderの初期化
-    // vertex shader用のプログラムをコンパイルする
     let vert_shader = shader::new_vertex_shader(&context)?;
-    // fragment shaderの初期化
-    // fragment shader用のプログラムをコンパイルする
     let frag_shader = shader::new_fragment_shader(&context)?;
-    // コンパイルしたshdaerプログラムをリンクする
     let program = processor::link_program(&context, &vert_shader, &frag_shader)?;
     context.use_program(Some(&program));
 
-    // テクスチャの作成とbind
     let texture = context.create_texture();
-    // context.active_texture(WebGlRenderingContext::TEXTURE0);
     context.bind_texture(WebGlRenderingContext::TEXTURE_2D, texture.as_ref());
 
-    // textureの設定（どんな画像サイズでも表示できるように）
     context.tex_parameteri(
         WebGlRenderingContext::TEXTURE_2D,
         WebGlRenderingContext::TEXTURE_WRAP_S,
@@ -215,12 +275,9 @@ pub async fn transition(data: Vec<u8>) -> Result<(), JsValue> {
         .collect::<Vec<&u8>>();
     let rgba = rgba.clone().into_iter().copied().collect::<Vec<u8>>();
 
-    // 読み込みビット？バイト？の単位を1にする（デフォルトは4）
     context.pixel_storei(WebGlRenderingContext::UNPACK_ALIGNMENT, 1);
 
-    // 画像データを流しこむ
     unsafe {
-        // let vert_array = js_sys::Uint8Array::view(&b);
         let vert_array = js_sys::Uint8Array::view(&rgba);
         let _ = context
             .tex_image_2d_with_i32_and_i32_and_i32_and_format_and_type_and_opt_array_buffer_view(
@@ -242,8 +299,7 @@ pub async fn transition(data: Vec<u8>) -> Result<(), JsValue> {
     let diff = width / 20;
     let mut i = 0;
     *g.borrow_mut() = Some(Closure::wrap(Box::new(move || {
-        if i >= width {
-            // body().set_text_content(Some("All done!"));
+        if i > width {
             let t = document.get_element_by_id("t").unwrap();
             t.set_text_content(Some("All done!"));
             let _ = f.borrow_mut().take();
@@ -253,19 +309,21 @@ pub async fn transition(data: Vec<u8>) -> Result<(), JsValue> {
         context.clear_color(0.0, 0.0, 0.0, 0.0);
         context.clear(WebGlRenderingContext::COLOR_BUFFER_BIT);
 
+        let x = if rev {
+            f32min(i as f32, width as f32)
+        } else {
+            f32max(-(i as f32), -(width as f32))
+        };
+        let y = 0.0;
+        unsafe {
+            log(format!("i:{}, x:{}, y:{}", i, x, y).as_str());
+        }
         // attributeの設定
         attribute::setup(
             &context,
             &program,
             "a_position",
-            (
-                if -(width as f32) > -(i as f32) {
-                    -(width as f32)
-                } else {
-                    -(i as f32)
-                },
-                0.0,
-            ),
+            (x, y),
             width as f32,
             height as f32,
         );
@@ -281,19 +339,22 @@ pub async fn transition(data: Vec<u8>) -> Result<(), JsValue> {
         );
         context.draw_arrays(WebGlRenderingContext::TRIANGLE_STRIP, 0, 6);
 
+        let x = if rev {
+            f32min(0f32, -((width - i) as f32))
+        } else {
+            f32max(0f32, (width - i) as f32)
+        };
+        let y = 0.0;
+        unsafe {
+            log(format!("i:{}, x:{}, y:{}", i, x, y).as_str());
+        }
+
         // attributeの設定
         attribute::setup(
             &context,
             &program,
             "a_position",
-            (
-                if 0f32 > (width - i) as f32 {
-                    0f32
-                } else {
-                    (width - i) as f32
-                },
-                0.0,
-            ),
+            (x, y),
             width as f32,
             height as f32,
         );
@@ -315,8 +376,6 @@ pub async fn transition(data: Vec<u8>) -> Result<(), JsValue> {
 
     request_animation_frame(g.borrow().as_ref().unwrap());
 
-    // request_animation_frame(&window, &f);
-
     Ok(())
 }
 
@@ -328,4 +387,20 @@ fn request_animation_frame(f: &Closure<dyn FnMut()>) {
     window()
         .request_animation_frame(f.as_ref().unchecked_ref())
         .expect("should register `requestAnimationFrame` OK");
+}
+
+fn f32max(a: f32, b: f32) -> f32 {
+    if a < b {
+        b
+    } else {
+        a
+    }
+}
+
+fn f32min(a: f32, b: f32) -> f32 {
+    if a < b {
+        a
+    } else {
+        b
+    }
 }
